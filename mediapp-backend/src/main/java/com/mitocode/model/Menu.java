3ae6bfd6a21fb489ba5fe0165901d9afbfcb1fd8
NestorPlasencia <nplasencia82@gmail.com>
2020-01-19 21:28:32 -0500

    modelos de etidades

---
+++ mediapp-backend/src/main/java/com/mitocode/model/Menu.java
@@ -0,0 +1,56 @@
+package com.mitocode.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="menu")
+public class Menu {
+	
+	@Id
+	private Integer idExamen;
+	
+	@Column(name="icono", length = 20)
+	private String icono;
+	
+	@Column(name="nombre", length = 20)
+	private String nombre;
+	
+	@Column(name="url", length = 50)
+	private String url;
+
+	public Integer getIdExamen() {
+		return idExamen;
+	}
+
+	public void setIdExamen(Integer idExamen) {
+		this.idExamen = idExamen;
+	}
+
+	public String getIcono() {
+		return icono;
+	}
+
+	public void setIcono(String icono) {
+		this.icono = icono;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
